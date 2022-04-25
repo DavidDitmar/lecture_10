6404fc3c2799ffc34dad45e2fdd3b6d8d131e1f6
David Ditmar <david.ditmar@seznam.cz>
2022-04-25 10:05:49 +0200

    selection_sort

--- sorting.py
+++ sorting.py
@@ -23,11 +23,34 @@ def read_data(file_name):
 
 
 
+def selection_sort(number_array, direction="ascending"):
+    n = len(number_array)
+    for i in range(n):
+        min_idx = i
+        for j in range(i+1, n):
+            if direction == "ascending":
+                if number_array[j] < number_array[min_idx]:
+                    min_idx = j
+            elif direction == "descending":
+                if number_array[j] > number_array[min_idx]:                     #staci jen zmenit znamenko
+                    min_idx = j
+
+        number_array[i], number_array[min_idx] = number_array[min_idx], number_array[i]
+
+    return number_array
+
+
+
+
+
+
 
 
 def main():
     data = read_data("numbers.csv")
-    print(data)
+    #print(data)
+    sorted_array = selection_sort(data["series_1"], "descending")
+    print(sorted_array)
 
 
 if __name__ == '__main__':
